503dfaf303e714654cf5bfca06b4eddd80e81f1a
kylepcox <kylepc13@gmail.com>
2022-01-12 15:30:23 -0600

    sidebar

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sportsreference.ncaab.teams import Teams
-import pickle
 from scipy.sparse.linalg import eigs
 from scipy.stats import norm
 from matplotlib import pyplot as plt
@@ -13,29 +12,13 @@ def get_teams():
     return Teams()
 teams = get_teams()
 
-hca = st.checkbox('Include home-court advantage')
-HCA_VAL = 2.5
+hca = st.sidebar.checkbox('Include home-court advantage')
+HCA_VAL = 1.25
 
-st.header('Change scoring function')
-st.write('  The idea behind all of this is an old one. I\'m using PageRank to rank college basketball teams. I encourage you to Google PageRank if you haven\'nt heard of it. \
-    You can imagine a graph of where each node is a team. The edges represent the games that have been played. We want to determine the value of those edges. \
-    The edge pointing from the winner to the loser should have a greater weight than the edge pointing from the loser to the winner.')
-
-st.write('I use a the margin of the game to inform the edge weights. A negative margin (you lost) is should yield a smaller edge weight than a positive margin (you won). \
-    But all our values of the edge weights need to be positive, both for the math to work and for the graph to make real sense. For the time being, I\'d like to suppose each game is weighted equally \
-        (though you can imagine other approaches to this: perhaps more recent games have higher weight, games with higher partiy have higher weight, etc.). \
-    So we want a function that both maps margin (which ranges from (-inf, +inf) to some bounded non-negative interval. We also want for this function f to be such that there exists an e such that for all margins in the reals there exists an e such that f(margin) + f(-margin) = e.  \
-    (This is the criterion that satsifies the weights of all games being equal.)')
-st.write('I use a logistic function of the margin of victory to determine the points alloted to any team. \
-            The function is value = 1 / (1 + np.exp(-k/100 * margin)). There are a couple nice things about this function. The first is that \
-            it f(margin) + f(-margin) = 1, so for all games 1 point is rewarded. Another thing is that you get diminishing returns for blowouts. The idea is that \
-            the difference between a 30 point win and 40 point win is less substantial than the difference between a 10 point win and a 20 point win. \
-            Another way to think about it is that the absolute value of the function is always concave down. \n \
-            I\'m leaving it open to the user to mess around with a value of k to see what works best. In my own rankings I manually adjust the value of k while paying attention to the tradeoff between wins predicted rate and wins explained rate (I call these predictive / retrodictive accuracy). \
-            There\'s certainly a better way to do it, and maybe it just requires me being more rigorous about my current method. But I thought it would be fun to make a toy where you can fiddle with this logistic function constant k and see what different rankings it yields.')
+st.sidebar.header('Change scoring function')
 
 # Let user change coefficient for logistic regression
-K = st.slider('Choose k.', min_value=0, max_value=100, value=10)
+K = st.sidebar.slider('Choose k.', min_value=0, max_value=100, value=10)
 margin_f = lambda margin: 1/ (1 + np.exp(-K/100 * margin))
 
 # Show (plot) result
@@ -49,12 +32,7 @@ ax.set_ylim(0, 1)
 ax.set_xlabel('margin')
 ax.set_ylabel('value rewarded (edge weight)')
 ax.set_title('reward = 1 / (1 + e^(-k/100 * margin))')
-st.pyplot(fig)
-
-# Introduce idea of editing future games
-st.write('Another thing I wanted to allow people to do was test out future games. I thought about listing all future games but there are too many to list nicely. \
-    Unfortunately, too, I could not figure out a way to display a sort of matrix of future games, or do anything graphically interesting. \
-    What I decided was the best solution was to allow the user to choose a team, and then see that team\'s future games, and edit those. ')
+st.sidebar.pyplot(fig)
 
 def get_percentile_rankings(rankings):
     std = np.std([i[1] for i in rankings.values()])
@@ -93,11 +71,6 @@ def load_data(teams_list):
             bs_index = game.boxscore_index if game.boxscore_index is not None else counter
             counter += 1
             data_dict[bs_index] = game_data
-
-    pickle_out = open("data_dict.pickle", "wb")
-    pickle.dump(data_dict, pickle_out)
-    pickle_out.close()
-
     return data_dict
 
 def get_adj_matrix(teams_list, data_dict):
@@ -191,7 +164,7 @@ def main():
     adj_matrix = get_adj_matrix(teams_list, data)
     future_games = load_future_games(data)
     future_games = pd.DataFrame.from_dict(future_games, orient='index', columns=['team1', 'team2', 'location', 'date'])
-    chosen_teams = st.multiselect('Select the teams whose games you want to edit', options=teams_list)
+    chosen_teams = st.sidebar.multiselect('Select the teams whose games you want to edit', options=teams_list)
     if len(chosen_teams) > 0:
         games_to_edit = future_games[future_games['team1'].isin(chosen_teams) | future_games['team2'].isin(chosen_teams)]
         # sort games_to_edit by date
@@ -202,9 +175,9 @@ def main():
         games_to_edit = games_to_edit.sort_values(by='sort_val')
         for boxscore_idx, game in games_to_edit.iterrows():
             home_team = game['team1'] if game['location'] == 'Home' else game['team2']
-            winner = st.radio('Choose winner: ' + str(str(game['date']).split(' ')[0]) + ' Home: ' + home_team, options=['None'] + [game['team1'], game['team2']], key=boxscore_idx)
+            winner = st.sidebar.radio('Choose winner: ' + str(str(game['date']).split(' ')[0]) + ' Home: ' + home_team, options=['None'] + [game['team1'], game['team2']], key=boxscore_idx)
             if winner != 'None':
-                margin = st.slider('Margin of victory', min_value=0, max_value=100, step=1, value=10, key=boxscore_idx)
+                margin = st.sidebar.slider('Margin of victory', min_value=0, max_value=100, step=1, value=10, key=boxscore_idx)
                 if hca:
                     if game['location'] == 'Home':
                         margin += HCA_VAL
@@ -224,6 +197,27 @@ def main():
     # change to str because of streamlit bug
     df = pd.DataFrame.from_dict(rankings, orient='index', columns = ['team', 'population adjusted rating', 'eigenvector rating']).astype(str)
     st.dataframe(df)
+
+    st.header('Motivation')
+    st.write('The idea behind all of this is an old one. I\'m using PageRank to rank college basketball teams. I encourage you to Google PageRank if you haven\'t heard of it. \
+             You can imagine a graph of where each node is a team. The edges represent the games that have been played. We want to determine the value of those edges. \
+            The edge pointing from the winner to the loser should have a greater weight than the edge pointing from the loser to the winner.')
+    st.write('I use a the margin of the game to inform the edge weights. A negative margin (you lost) is should yield a smaller edge weight than a positive margin (you won). \
+            But all our values of the edge weights need to be positive, both for the math to work and for the graph to make real sense. For the time being, I\'d like to suppose each game is weighted equally \
+            (though you can imagine other approaches to this: perhaps more recent games have higher weight, games with higher partiy have higher weight, etc.). \
+            So we want a function that both maps margin (which ranges from (-inf, +inf) to some bounded non-negative interval. We also want for this function f to be such that there exists an e such that for all margins in the reals there exists an e such that f(margin) + f(-margin) = e.  \
+            (This is the criterion that satsifies the weights of all games being equal.)')
+    st.write('I use a logistic function of the margin of victory to determine the points alloted to any team. \
+            The function is value = 1 / (1 + np.exp(-k/100 * margin)). There are a couple nice things about this function. The first is that \
+            it f(margin) + f(-margin) = 1, so for all games 1 point is rewarded. Another thing is that you get diminishing returns for blowouts. The idea is that \
+            the difference between a 30 point win and 40 point win is less substantial than the difference between a 10 point win and a 20 point win. \
+            Another way to think about it is that the absolute value of the function is always concave down. \n \
+            I\'m leaving it open to the user to mess around with a value of k to see what works best. In my own rankings I manually adjust the value of k while paying attention to the tradeoff between wins predicted rate and wins explained rate (I call these predictive / retrodictive accuracy). \
+            There\'s certainly a better way to do it, and maybe it just requires me being more rigorous about my current method. But I thought it would be fun to make a toy where you can fiddle with this logistic function constant k and see what different rankings it yields.') 
+    # Introduce idea of editing future games
+    st.write('Another thing I wanted to allow people to do was test out future games. I thought about listing all future games but there are too many to list nicely. \
+            Unfortunately, too, I could not figure out a way to display a sort of matrix of future games, or do anything graphically interesting. \
+            What I decided was the best solution was to allow the user to choose a team, and then see that team\'s future games, and edit those. ')      
 
 if __name__ == '__main__':
     main()
